0daeaf384683e1e2a82bf0c11c278e90c5519782
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-02-24 14:38:53 +0300

    docker nginx

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -6,8 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/test/',
-  publicPath: '/test/',
+  base: './',
+  publicPath: './',
   plugins: [
     vue(),
     vueJsx(),
