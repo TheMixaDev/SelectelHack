b8e80d10bf26470d53b9a3eaa3916a285d30339d
Kirill Parkhomenko <kxrxh.contact@gmail.com>
2024-02-25 00:07:36 +0300

    case fix

--- backend/database/donation_plan.go
+++ backend/database/donation_plan.go
@@ -7,7 +7,7 @@ import (
 )
 
 func AddDonationPlan(userId uint, donationPlan DonationPlan) (uint, error) {
-	row := database.QueryRow(context.Background(), "INSERT INTO donation_plan (user_id, blood_station_id, city_id, donate_at, blood_class, payment_type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
+	row := database.QueryRow(context.Background(), "INSERT INTO donationPlan (user_id, blood_station_id, city_id, donate_at, blood_class, payment_type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
 		userId, donationPlan.BloodStationID, donationPlan.CityID, donationPlan.DonateAt, donationPlan.BloodClass, donationPlan.PaymentType)
 	err := row.Scan(&donationPlan.ID)
 	if err != nil {
@@ -20,7 +20,7 @@ func AddDonationPlan(userId uint, donationPlan DonationPlan) (uint, error) {
 }
 
 func DeleteDonationPlan(id uint) error {
-	_, err := database.Exec(context.Background(), "DELETE FROM donation_plan WHERE id = $1", id)
+	_, err := database.Exec(context.Background(), "DELETE FROM donationPlan WHERE id = $1", id)
 	if err != nil {
 		zap.S().Warnln("ERROR while deleting donationPlan", zap.Error(err))
 		return err
