866c98dbaa085924f4a464f4dc36ae650ceb5793
aleph <20299694+TheMixaDev@users.noreply.github.com>
2024-02-25 04:29:31 +0300

    remove debug

--- webapp/src/views/authorized/donations/DonationsView.vue
+++ webapp/src/views/authorized/donations/DonationsView.vue
@@ -8,9 +8,6 @@ import UITableEmpty from '@/components/ui/table/UITableEmpty.vue';
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <p>
-                        {{ token }}
-                    </p>
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
                         Мои донации
                     </h1>
@@ -30,8 +27,7 @@ export default {
     name: 'DonationsView',
     data() {
         return {
-            donations: [],
-            token: ""
+            donations: []
         };
     },
     methods: {
@@ -41,7 +37,6 @@ export default {
         }
     },
     mounted() {
-        this.token = this.$cookies.get("token");
         DonationService.getDonations((data) => {
             this.donations = data;
         }, () => {
