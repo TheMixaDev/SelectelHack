ddea20bf1c1c23b8cf1b19936fbc9bb5fd7375b2
isofinly <isofinly@icloud.com>
2024-02-25 03:21:49 +0300

    idk

--- backend/database/bonuses.go
+++ backend/database/bonuses.go
@@ -1,5 +1,18 @@
 package database
 
+import (
+	"context"
+	"strconv"
+)
+
+// AddFeedback updates the feedback for a bonus.
 func AddFeedback(id int, rating int, comment string) error {
-	return nil // TODO: IMPLEMET
+	// Prepare the SQL statement.
+	sqlStatement := `UPDATE Bonus SET rating = $1, comment = $2, has_feedback = true WHERE id = $3;`
+	// Execute the SQL statement.
+	_, err := database.Exec(context.Background(), sqlStatement, strconv.Itoa(rating), comment, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- config/k8s/start.sh
+++ config/k8s/start.sh
@@ -3,20 +3,24 @@
 echo "Starting minikube..."
 minikube start
 
+sleep 15
+
 # cockroachdb
 echo 'Creating cockroach cluster...'
 kubectl create -f ./cockroachdb/config.yaml
 
-sleep 5
+sleep 10
 
 echo 'Creating cockroach load balancer...'
 kubectl create -f ./cockroachdb/lb.yaml
+
+sleep 15
 
 # redis
 echo 'Creating redis cluster...'
 kubectl apply -f ./redis/config.yaml
 
-sleep 10
+sleep 15
 
 # minio
 echo 'Creating minio cluster...'
